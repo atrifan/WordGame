79f639938b056841d2f50f33348af35b0036f846
Mihăiţă GHIORGHE <mihaitaghiorghe@gmail.com>
2015-11-10 08:05:06 -0800

    Implemented dictionary

--- WordGameServer/src/main/java/ro/mps/wordsgame/logic/Dictionary.java
+++ WordGameServer/src/main/java/ro/mps/wordsgame/logic/Dictionary.java
@@ -1,11 +1,54 @@
 package ro.mps.wordsgame.logic;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util;
+
 /**
  * Created by alexandru.trifan on 27.10.2015.
  */
+
 public class Dictionary {
 
+	private HashMap<String> wordSet;
+
+	public Dictionary() throws Exception {
+		try {
+			readWords();
+		}
+		
+		catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	private void readWords() throws Exception {
+		try {
+			File inputFile = new File("words");
+			BufferedReader in = new BufferedReader(
+					   	  new InputStreamReader(
+			                    new FileInputStream(inputFile), "UTF8"));
+			
+			String str;
+		      
+			while ((str = in.readLine()) != null) {
+			    wordSet.add(str.toLowerCase());
+			}
+			
+			in.close();
+		}
+		
+		catch(Exception e) {
+			throw e;
+		}
+	}
+	
     public boolean isValid(String word) {
-        return true;
+        return wordSet.contains(word);
     }
 }
+
